a4530a1159a6e3de6cb144b3dff7515802a1f938
Sammy-T <cyam9019@gmail.com>
2024-06-30 07:08:40 -0400

    Decrypt vault contents

--- cmd/avdu/main.go
+++ cmd/avdu/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"encoding/base64"
 	"encoding/hex"
 	"errors"
 	"log"
@@ -31,6 +32,13 @@ func main() {
 	}
 
 	log.Printf("Found master key: %v", masterKey)
+
+	content, err := decryptContents(vaultDataEnc.Db, vaultDataEnc.Header.Params, masterKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Decrypted:\n\n%v", string(content))
 }
 
 func findMasterKey(slots []vault.Slot, pwd string) ([]byte, error) {
@@ -45,38 +53,38 @@ func findMasterKey(slots []vault.Slot, pwd string) ([]byte, error) {
 
 		salt, err := hex.DecodeString(slot.Salt)
 		if err != nil {
-			log.Fatal(err)
+			return nil, err
 		}
 
 		// Create a key using the slot values and provided password
 		key, err = scrypt.Key([]byte(pwd), salt, slot.N, slot.R, slot.P, 32)
 		if err != nil {
-			log.Fatal(err)
+			return nil, err
 		}
 
 		block, err := aes.NewCipher(key)
 		if err != nil {
-			log.Fatal(err)
+			return nil, err
 		}
 
 		aesgcm, err := cipher.NewGCM(block)
 		if err != nil {
-			log.Fatal(err)
+			return nil, err
 		}
 
 		nonce, err := hex.DecodeString(slot.KeyParams.Nonce)
 		if err != nil {
-			log.Fatal(err)
+			return nil, err
 		}
 
 		tag, err := hex.DecodeString(slot.KeyParams.Tag)
 		if err != nil {
-			log.Fatal(err)
+			return nil, err
 		}
 
 		slotKey, err := hex.DecodeString(slot.Key)
 		if err != nil {
-			log.Fatal(err)
+			return nil, err
 		}
 
 		var keyData []byte = append(slotKey, tag...)
@@ -93,4 +101,41 @@ func findMasterKey(slots []vault.Slot, pwd string) ([]byte, error) {
 	}
 
 	return masterKey, nil
+}
+
+func decryptContents(db string, params vault.Params, masterKey []byte) ([]byte, error) {
+	block, err := aes.NewCipher(masterKey)
+	if err != nil {
+		return nil, err
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce, err := hex.DecodeString(params.Nonce)
+	if err != nil {
+		return nil, err
+	}
+
+	tag, err := hex.DecodeString(params.Tag)
+	if err != nil {
+		return nil, err
+	}
+
+	dbData, err := base64.StdEncoding.DecodeString(db)
+	if err != nil {
+		return nil, err
+	}
+
+	var database []byte = append(dbData, tag...)
+
+	// Attempt to decrypt the vault content
+	content, err := aesgcm.Open(nil, nonce, database, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return content, nil
 }

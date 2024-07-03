2f5f0567beeabac2efde6c1b95b19f2a0a226e99
Sammy-T <cyam9019@gmail.com>
2024-07-02 21:27:03 -0400

    Implement auto refreshing display

--- cmd/avdu/main.go
+++ cmd/avdu/main.go
@@ -15,11 +15,16 @@ import (
 )
 
 const timeFmt string = "2006/01/02 15:04:05"
+const defPeriod int64 = 30 // The default TOTP refresh interval
+const refreshLimit int = 50
+
+var refreshed int
 
 func main() {
 	app := &cli.App{
-		Name:  "avdu",
-		Usage: "Read an Aegis vault backup or export file.",
+		Name:    "avdu",
+		Usage:   "Read an Aegis vault backup or export file.",
+		Version: "0.1.0",
 		Flags: []cli.Flag{
 			&cli.PathFlag{
 				Name:    "path",
@@ -32,8 +37,13 @@ func main() {
 				Aliases: []string{"pwd", "k"},
 				Usage:   "specify the password used to decrypt the vault",
 			},
+			&cli.BoolFlag{
+				Name:    "refresh",
+				Aliases: []string{"r"},
+				Usage:   "automatically refreshes the OTP display",
+			},
 		},
-		Action: rootAction,
+		Action: cliAction,
 	}
 
 	if err := app.Run(os.Args); err != nil {
@@ -41,7 +51,7 @@ func main() {
 	}
 }
 
-func rootAction(ctx *cli.Context) error {
+func cliAction(ctx *cli.Context) error {
 	var path string = ctx.Path("path")
 
 	isFilePath, err := regexp.MatchString(`.json`, path)
@@ -77,6 +87,23 @@ func rootAction(ctx *cli.Context) error {
 
 	fmt.Printf("%v Read file: %v\n", time.Now().Format(timeFmt), vaultPath)
 
+	displayOTPs(vaultData)
+
+	var ch chan int = make(chan int)
+
+	for ctx.Bool("refresh") && refreshed < refreshLimit {
+		go displayCountdown(ch)
+		<-ch // Block progression by waiting to receive data on the channel
+
+		log.Println("Refreshed OTPs")
+		displayOTPs(vaultData)
+	}
+
+	return nil
+}
+
+// displayOTPs is a helper to output the OTP data.
+func displayOTPs(vaultData *vault.Vault) {
 	otps, err := avdu.GetOTPs(vaultData)
 	if err != nil {
 		log.Println(err)
@@ -91,8 +118,27 @@ func rootAction(ctx *cli.Context) error {
 	}
 
 	fmt.Printf("%v %v\n", time.Now().Format(timeFmt), builder.String())
+}
 
-	return nil
+// displayCountdown is a helper that outputs a countdown to the same line
+// then transmits data to the channel when the coundown finishes.
+func displayCountdown(ch chan int) {
+	var p int64 = defPeriod * 1000
+
+	// Calculate the time til next refresh
+	var ttn int64 = p - (time.Now().UnixMilli() % p)
+
+	for ttn > 1000 {
+		fmt.Printf("\rRefreshes in %vs ", float32(ttn)/1000)
+
+		ttn = p - (time.Now().UnixMilli() % p)
+
+		time.Sleep(1 * time.Second)
+	}
+
+	fmt.Println() // Ensure there's a fresh line for additional output
+
+	ch <- 0 // Return arbitrary data to free up the channel
 }
 
 // findVaultPath is a helper that returns the most recently modified
